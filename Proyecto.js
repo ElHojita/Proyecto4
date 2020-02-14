99a4ecc9ceb07eebef7a82aacd75e25f16c99e5d
ElHojita <ssilvestreo1@miumg.edu.gt>
2020-02-13 22:09:45 -0600

    Add validation

--- Proyecto.js
+++ Proyecto.js
@@ -1,16 +1,29 @@
 var fechadenacimiento,fechahoy,varmes, d = new Date();
+varmes=parseInt(d.getMonth())+1; //Obtiene el valor del mes actual restandole uno, ejemplo febrero (2), lo toma como enero (1), por tanto se le suma uno 
+fechahoy=(d.getDate()+','+varmes.toString()+','+d.getFullYear()).split(',');
 var nombreCompleto = prompt("Por favor ingrese el nombre completo:", "Nombres y apellidos");
 var diaNacimiento = prompt("Ingrese el día de nacimiento", "Ejemplo: Si nació el 22 de enero, solamente ingresar 22");
 var mesNacimiento = prompt("Ingrese el mes de nacimiento", "Ejemplo: Si nació en enero, solamente ingresar 1 porque es el mes 1");
 var anioNacimiento = prompt("Ingrese el año de nacimiento", "Ejemplo: Si nació el 22 de enero de 1987, solamente ingresar 1987");
+if(Calculoedad(diaNacimiento+','+mesNacimiento+','+anioNacimiento)==true){ //If mayoria de edad
 var conyuge = prompt("¿Tiene cónyuge?", "SI/NO");
 var hijos = prompt("¿Tiene hijos?", "SI/NO");
+conyuge =conyuge.toLowerCase();
+hijos =hijos.toLowerCase();
+document.write(hijos);
+if(hijos=='si')
 var cantidadHijos = prompt("Ingrese la cantidad de hijos menores de 21 años:", "Por favor ingrese únicamente números");
+}//If mayoria de edad
+else{
+  alert('No cumple los requisitos de edad');
+}
 
-conyuge.toLowerCase();
-hijos.toLowerCase();
-varmes=parseInt(d.getMonth())+1; //Obtiene el valor del mes actual restandole uno, ejemplo febrero (2), lo toma como enero (1), por tanto se le suma uno 
-fechahoy=(d.getDate()+','+varmes.toString()+','+d.getFullYear()).split(',');
+
+
+
+
+
+
 
 
 
